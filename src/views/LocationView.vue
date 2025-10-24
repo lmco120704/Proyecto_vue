1358cf0cc1950be35d5cc0f84afc9bd808ce854e
Luis Miguel Candanedo Olmos <lmco120704@gmail.com>
2025-10-24 00:23:15 -0600

    cambio de mapa

--- src/views/LocationView.vue
+++ src/views/LocationView.vue
@@ -8,7 +8,7 @@
       <div class="col-12">
         <!-- Google Maps Embed -->
         <div class="shadow-lg">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.521260322283!2d-75.5677439857393!3d6.200995995512335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e442658d8a26d5f%3A0x305dbcb50684833!2sParque%20Lleras%2C%20Medell%C3%ADn%2C%20Antioquia%2C%20Colombia!5e0!3m2!1sen!2sus!4v1678886135332!5m2!1sen!2sus" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9051.788898518402!2d-96.8401002840388!3d19.94934987168643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db117ae7e06483%3A0x8322b670e06e8cb3!2sInstituto%20Tecnol%C3%B3gico%20Superior%20de%20Misantla!5e0!3m2!1ses-419!2smx!4v1761286709796!5m2!1ses-419!2smx" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
       <div class="col-12 text-center mt-4">
